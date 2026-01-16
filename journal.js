6ebd2a808775378bf26502599e62f8037ca5bc70
Alan Vasquez (Orvenx) <vazquezzmooran@gmail.com>
2026-01-15 22:15:44 -0800

    Add journal.js with 5 JavaScript functions

---
+++ journal.js
@@ -0,0 +1,24 @@
+// 1. Greet a user by name
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+// 2. Check if a number is even
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+// 3. Calculate the sum of two numbers
+function add(a, b) {
+  return a + b;
+}
+
+// 4. Convert Celsius to Fahrenheit
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+// 5. Find the maximum of two numbers
+function findMax(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
